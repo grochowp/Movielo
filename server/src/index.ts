65de49c9c0078b125df1c8f947054fc97e7761fb
grochowp <patgro3@wp.pl>
2024-02-18 19:13:00 +0100

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+// src/index.js
+import express, { Request, Response } from "express";
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
